108453cfda985557d525f4367c70f9d04857cfa9
snehadubey9066 <59720911+snehadubey9066@users.noreply.github.com>
2020-01-10 10:57:26 +0530

    Update first.c

--- first.c
+++ first.c
@@ -6,11 +6,12 @@ void main()
     int fib[N];
     fib[0]=0;
     fib[1]=1;
-    for(int i=2;i<N;i++)
+    int i;
+    for(i=2;i<N;i++)
     {
         fib[i]=fib[i-1]+fib[i-2];
     }
-    for(int i=0;i<N;i++)
+    for(i=0;i<N;i++)
     {
         printf("fib[%d] is: %d\n",i,fib[i]);
     }
